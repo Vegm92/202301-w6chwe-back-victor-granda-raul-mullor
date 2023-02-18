9dae966b45e9e36b455239689811aee734a3b119
Victor Granda <victorgranda1992@gmail.com>
2023-02-18 19:17:30 +0100

    Add errorMiddlewares.test and jest.config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,8 +8,22 @@ module.exports = {
     {
       extends: ["xo-typescript", "prettier"],
       files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+        "@typescript-eslint/consistent-type-assertions": "off",
+      },
+    },
+    {
+      files: ["src/**/models/**/*.ts", "src/**/*.ts"],
+      rules: {
+        "@typescript-eslint/naming-convention": "off",
+      },
     },
   ],
+
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
