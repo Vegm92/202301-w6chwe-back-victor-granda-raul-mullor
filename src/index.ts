e5e0e88cd745014f51b397dc21468b8aa3d2f667
Victor Granda <victorgranda1992@gmail.com>
2023-02-19 17:40:46 +0100

    Add error if server is unavailable

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import startServer from "./server/startServer.js";
 
 const port = process.env.PORT ?? 4000;
 const mongoDbUrl = process.env.MONGODB_CONNECTION_URL!;
-await connectDatabase(mongoDbUrl);
 await startServer(+port);
+await connectDatabase(mongoDbUrl);
